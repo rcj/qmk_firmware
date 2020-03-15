dd84c9b34b6c6d0038d402cd66109a7cb7b75a55
Robin Campbell Joy <rcj@robinjoy.net>
2020-03-15 15:04:58 +0100

    Add LSFT/LCTRL to lets split Movement layer

--- keyboards/lets_split_eh/keymaps/rcj/keymap.c
+++ keyboards/lets_split_eh/keymaps/rcj/keymap.c
@@ -85,8 +85,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_MOVEMENT] = LAYOUT( \
   _______, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_HOME, KC_0,    KC_7,    KC_8,    KC_9,    _______, _______, \
-  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_DOT,  KC_4,    KC_5,    KC_6,    _______, _______, \
-  _______, _______, KC_INS,  _______, _______, KC_PSCR, KC_COMM, KC_1,    KC_2,    KC_3,    _______, _______, \
+  _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_DOT,  KC_4,    KC_5,    KC_6,    _______, _______, \
+  _______, KC_LCTL, KC_INS,  _______, _______, KC_PSCR, KC_COMM, KC_1,    KC_2,    KC_3,    _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
