c37255599f266b6f9354e2973fd66b07b8211a54
Robin Campbell Joy <rcj@robinjoy.net>
2020-08-24 00:43:14 +0200

    Add FFXIV Layer

--- keyboards/ergodox_ez/keymaps/rcj/keymap.c
+++ keyboards/ergodox_ez/keymaps/rcj/keymap.c
@@ -7,6 +7,7 @@
 
 enum rcj_layers {
   _QWERTY = 0,
+  _FFXIV,
   _MOVEMENT,
   _BRACES,
   _SYMBOLS,
@@ -17,7 +18,29 @@ enum rcj_keycodes {
   QWERTY = SAFE_RANGE,
   SYM_CBR,
   SYM_PRN,
-  SYM_BRC
+  SYM_BRC,
+  FF_1,
+  FF_2,
+  FF_3,
+  FF_4,
+  FF_5,
+  FF_6,
+  FF_7,
+  FF_8,
+  FF_9,
+  FF_0,
+  FF_Q,
+  FF_W,
+  FF_R,
+  FF_T,
+  FF_A,
+  FF_G,
+  FF_Z,
+  FF_X,
+  FF_C,
+  FF_V,
+  FF_B,
+  FF_LSFTF
 };
 
 #define MOD_SHIFT_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
@@ -85,11 +108,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CME, KC_A,    KC_S,    KC_D,        KC_F,      KC_G,
         KC_SCD, KC_Z,    KC_X,    KC_C,        KC_V,      KC_B,   _______,
         KC_ESC, _______, _______, KC_MOV,      KC_LCTL,
-                                                           _______, _______,
+                                                     TG(_FFXIV), TG(_FFXIV),
                                                                     _______,
-                                                   KC_SPC, KC_ESC,  _______,
+                                                   KC_SPC, KC_LALT, _______,
         // right hand
              _______, KC_6, KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+             _______, KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    _______,
+                      KC_H, KC_J,    KC_K,    KC_L,    KC_SYM,  KC_QUOT,
+             _______, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_BRSL, KC_HYPR,
+                            KC_LALT, KC_LGUI, _______, MO(_ADJUST),          KC_ESC,
+             _______, _______,
+             _______,
+             _______, LCTL(KC_C), KC_LSFT
+    ),
+[_FFXIV] = LAYOUT_ergodox(
+        // left hand
+        KC_ESC, FF_1,    FF_2,    FF_3,        FF_4,      FF_5,   _______,
+        KC_TAB, FF_Q,    FF_W,    KC_E,        FF_R,      FF_T,   FF_LSFTF,
+        KC_CME, FF_A,    KC_S,    KC_D,        KC_F,      FF_G,
+        KC_SCD, FF_Z,    FF_X,    FF_C,        FF_V,      FF_B,   _______,
+        KC_ESC, _______, _______, KC_LALT,     KC_LCTL,
+                                                   TG(_FFXIV), TG(_FFXIV),
+                                                                    _______,
+                                                   KC_SPC, KC_LALT, _______,
+        // right hand
+             _______, FF_6, FF_7,    FF_8,    FF_9,    FF_0,    KC_BSPC,
              _______, KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    _______,
                       KC_H, KC_J,    KC_K,    KC_L,    KC_SYM,  KC_QUOT,
              _______, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_BRSL, KC_HYPR,
@@ -129,9 +172,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   _______, _______, _______,
     // right hand
        _______,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       _______,  KC_0,    KC_7,    KC_8,    KC_9,    _______, KC_F12,
-                 KC_DOT,  KC_4,    KC_5,    KC_6,    _______, _______,
-       _______,  KC_COMM, KC_1,    KC_2,    KC_3,    _______, _______,
+       _______,  KC_KP_0, KC_KP_7, KC_KP_8, KC_KP_9, KC_PMNS, KC_F12,
+                 KC_DOT,  KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, KC_F13,
+       _______,  KC_COMM, KC_KP_1, KC_KP_2, KC_KP_3, KC_PAST, KC_F14,
                           _______, _______, _______, _______, _______,
        _______, _______,
        _______,
@@ -249,6 +292,35 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 };
 
 bool shifted = false;
+static uint16_t key_to_repeat = 0;
+static uint32_t repeat_timer = 0;
+
+uint16_t get_key_to_repeat(uint16_t keycode) {
+    switch (keycode) {
+        case FF_Q: return KC_Q;
+        case FF_W: return KC_W;
+        case FF_R: return KC_R;
+        case FF_T: return KC_T;
+        case FF_A: return KC_A;
+        case FF_G: return KC_G;
+        case FF_Z: return KC_Z;
+        case FF_X: return KC_X;
+        case FF_C: return KC_C;
+        case FF_V: return KC_V;
+        case FF_B: return KC_B;
+        case FF_1: return KC_1;
+        case FF_2: return KC_2;
+        case FF_3: return KC_3;
+        case FF_4: return KC_4;
+        case FF_5: return KC_5;
+        case FF_6: return KC_6;
+        case FF_7: return KC_7;
+        case FF_8: return KC_8;
+        case FF_9: return KC_9;
+        case FF_0: return KC_0;
+    }
+    return 0;
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -280,6 +352,54 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
         }
+        case FF_Q:
+        case FF_W:
+        case FF_R:
+        case FF_T:
+        case FF_A:
+        case FF_G:
+        case FF_Z:
+        case FF_X:
+        case FF_C:
+        case FF_V:
+        case FF_B:
+        case FF_1:
+        case FF_2:
+        case FF_3:
+        case FF_4:
+        case FF_5:
+        case FF_6:
+        case FF_7:
+        case FF_8:
+        case FF_9:
+        case FF_0:
+            if (record->event.pressed) {
+                key_to_repeat = get_key_to_repeat(keycode);
+                register_code(key_to_repeat);
+                unregister_code(key_to_repeat);
+                repeat_timer = timer_read32();
+              } else {
+                  if (key_to_repeat == get_key_to_repeat(keycode))
+                      key_to_repeat = 0;
+          }
+          return false;
+        case FF_LSFTF:
+            if (record->event.pressed) {
+                register_code(KC_LSFT);
+                register_code(KC_F);
+            } else {
+                unregister_code(KC_F);
+                unregister_code(KC_LSFT);
+            }
+	    return false;
     }
     return true;
+}
+
+void matrix_scan_user(void) {
+    if (key_to_repeat && timer_elapsed32(repeat_timer) > 100) {
+        register_code(key_to_repeat);
+        unregister_code(key_to_repeat);
+        repeat_timer = timer_read32();
+    }
 }
